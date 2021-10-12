75f949f80a5671a6e2f9b506cec4efbbc18e3f0e
zagaynov-andrew <zagaynov.drew@gmail.com>
2021-10-12 22:10:46 +0300

    Add : utils : toUpper()

---
+++ utils.cpp
@@ -0,0 +1,10 @@
+#include "utils.hpp"
+
+const std::string   utils::toUpper( const std::string &str )
+{
+    std::string   tmp(str.length(), 0);
+
+    for (size_t i = 0; i < str.length(); ++i)
+        tmp[i] = std::toupper(str[i]);
+    return (tmp);
+}
