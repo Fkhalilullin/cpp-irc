f904dde29f64f0eeedb17fba170dc6ac3a9d60f1
Zagaynov Andrew <zagaynov.drew@gmail.com>
2021-10-20 17:56:43 +0300

    Add : base of the bot

---
+++ bot/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Client.hpp"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        std::cerr << "Invalid number of arguments" << std::endl;
+        return (1);
+    }
+    try
+    {
+        Client c(argv[1], argv[2], argv[3]);
+        c.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    return (0);
+}
